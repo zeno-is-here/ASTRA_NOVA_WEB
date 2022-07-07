a32dfde6f3dc834872a42c9b09ebf3d93a2f6622
kg-kartik <goel.kartik39@gmail.com>
2022-07-08 02:40:25 +0530

    cra setup with tailwind

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import About from './components/About';
+import Faq from './components/Faq';
+import Hero from './components/Hero';
+import MidSection from './components/MidSection';
+import Navbar from './components/Navbar';
+import Example from './components/Team';
+import './styles.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Hero />
+      <MidSection />
+      <About />
+      <Example />
+      <Faq />
+    </>
   );
 }
 
