333be0a1196f5d253ca92561d3796edb55de2551
kg-kartik <goel.kartik39@gmail.com>
2022-07-08 02:40:00 +0530

    Added layouts and css for other sections

---
+++ src/components/Faq.jsx
@@ -0,0 +1,77 @@
+/* This example requires Tailwind CSS v2.0+ */
+import { Disclosure } from '@headlessui/react';
+import { ChevronDownIcon } from '@heroicons/react/outline';
+
+const faqs = [
+  {
+    question: "What's the best thing about Switzerland?",
+    answer:
+      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+  },
+  {
+    question: "What's the best thing about Switzerland?",
+    answer:
+      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+  },
+  {
+    question: "What's the best thing about Switzerland?",
+    answer:
+      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+  },
+  {
+    question: "What's the best thing about Switzerland?",
+    answer:
+      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+  },
+  {
+    question: "What's the best thing about Switzerland?",
+    answer:
+      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+  },
+  // More questions...
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ');
+}
+
+export default function Faq() {
+  return (
+    <div className="text-white">
+      <div className="max-w-7xl mx-[8%] py-12 px-4 sm:py-16 sm:px-6 lg:px-8">
+        <h2 className=" text-3xl font-extrabold  sm:text-[40px] text-[#E0E0E0] uppercase">
+          Frequently asked questions?
+        </h2>
+        <dl className="mt-6 space-y-6 divide-y">
+          {faqs.map((faq) => (
+            <Disclosure as="div" key={faq.question} className="pt-6">
+              {({ open }) => (
+                <>
+                  <dt className="text-lg">
+                    <Disclosure.Button className="text-left w-full flex justify-between items-start text-gray-400">
+                      <span className="font-medium text-[#ffffff]">
+                        {faq.question}
+                      </span>
+                      <span className="ml-6 h-7 flex items-center">
+                        <ChevronDownIcon
+                          className={classNames(
+                            open ? '-rotate-180' : 'rotate-0',
+                            'h-6 w-6 transform'
+                          )}
+                          aria-hidden="true"
+                        />
+                      </span>
+                    </Disclosure.Button>
+                  </dt>
+                  <Disclosure.Panel as="dd" className="mt-2 pr-12">
+                    <p className="text-base text-[#595959]">{faq.answer}</p>
+                  </Disclosure.Panel>
+                </>
+              )}
+            </Disclosure>
+          ))}
+        </dl>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Team.jsx
@@ -0,0 +1,78 @@
+/*
+  This example requires Tailwind CSS v2.0+ 
+  
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/aspect-ratio'),
+    ],
+  }
+  ```
+*/
+const people = [
+  {
+    name: 'Lindsay Walton',
+    role: 'Front-end Developer',
+    imageUrl:
+      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+  },
+  {
+    name: 'Lindsay Walton',
+    role: 'Front-end Developer',
+    imageUrl:
+      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+  },
+  {
+    name: 'Lindsay Walton',
+    role: 'Front-end Developer',
+    imageUrl:
+      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+  },
+  // More people...
+];
+
+export default function Example() {
+  return (
+    <div className="text-white">
+      <div className="mx-auto py-12 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24">
+        <div className="space-y-12">
+          <div className="space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none">
+            <h2 className="text-3xl font-extrabold tracking-tight sm:text-4xl text-center">
+              Our Team
+            </h2>
+          </div>
+          <ul
+            role="list"
+            className="space-y-12 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:grid-cols-3 lg:gap-x-8"
+          >
+            {people.map((person) => (
+              <li key={person.name}>
+                <div className="space-y-4">
+                  <div className="aspect-w-3 aspect-h-2">
+                    <img
+                      className="object-cover shadow-lg rounded-lg"
+                      src={person.imageUrl}
+                      alt=""
+                    />
+                  </div>
+
+                  <div className="space-y-2 text-center">
+                    <div className="text-lg leading-6 font-medium space-y-1">
+                      <h3>{person.name}</h3>
+                      <p className="text-indigo-600">{person.role}</p>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
