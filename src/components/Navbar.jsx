9c6a751c33ddefb3a42f8723233570ee9f7bbdfe
kg-kartik <goel.kartik39@gmail.com>
2022-07-11 18:07:49 +0530

    some more changes :)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,127 +1,112 @@
 /* This example requires Tailwind CSS v2.0+ */
-import { Fragment } from "react";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
-import { PlusSmIcon } from "@heroicons/react/solid";
+import { Fragment } from 'react';
+import { Disclosure, Menu, Transition } from '@headlessui/react';
+import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline';
+import { PlusSmIcon } from '@heroicons/react/solid';
 
 const user = {
-	name: "Tom Cook",
-	email: "tom@example.com",
-	imageUrl:
-		"https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  name: 'Tom Cook',
+  email: 'tom@example.com',
+  imageUrl:
+    'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
 };
 const navigation = [
-	{ name: "Astra Nova", href: "#" },
-	{ name: "Nft Collections", href: "#" },
-	{ name: "Deviants", href: "#" },
-	{ name: "Meta RPG", href: "#" },
-	{ name: "Social", href: "#" },
-	{ name: "Wallpaper", href: "#" },
+  { name: 'Astra Nova', href: '#' },
+  { name: 'Nft Collections', href: '#' },
+  { name: 'Deviants', href: '#' },
+  { name: 'Meta RPG', href: '#' },
+  { name: 'Social', href: '#' },
+  { name: 'Wallpaper', href: '#' },
 ];
 function classNames(...classes) {
-	return classes.filter(Boolean).join(" ");
+  return classes.filter(Boolean).join(' ');
 }
 
 export default function Example() {
-	return (
-		<Disclosure as="nav">
-			{({ open }) => (
-				<>
-					<div
-						className="pt-2"
-						style={{
-							background:
-								"linear-gradient(180deg, rgba(0,0,0,1) 0%, rgba(255,255,255,0) 100%)",
-						}}>
-						<div className="flex justify-between h-16 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-							<div className="flex">
-								<div className="-ml-2 mr-2 flex items-center md:hidden">
-									{/* Mobile menu button */}
-									<Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-										<span className="sr-only">
-											Open main menu
-										</span>
-										{open ? (
-											<XIcon
-												className="block h-6 w-6"
-												aria-hidden="true"
-											/>
-										) : (
-											<MenuIcon
-												className="block h-6 w-6"
-												aria-hidden="true"
-											/>
-										)}
-									</Disclosure.Button>
-								</div>
-								<div className="flex-shrink-0 flex items-center">
-									<img
-										className="block lg:hidden h-8 w-auto"
-										src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-										alt="Workflow"
-									/>
-									<img
-										className="hidden lg:block h-8 w-auto"
-										src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
-										alt="Workflow"
-									/>
-								</div>
-								<div className="hidden md:ml-6 md:flex md:items-center md:space-x-4">
-									{navigation.map((item) => (
-										<a
-											key={item.name}
-											href={item.href}
-											className={classNames(
-												"text-gray-300 hover:bg-gray-700 hover:text-white",
-												"px-3 py-2 rounded-md text-base font-medium"
-											)}
-											aria-current={
-												item.current
-													? "page"
-													: undefined
-											}>
-											{item.name}
-										</a>
-									))}
-								</div>
-							</div>
-							<div className="flex items-center">
-								<div className="flex-shrink-0">
-									<button
-										type="button"
-										className="relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-500 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-indigo-500">
-										<PlusSmIcon
-											className="-ml-1 mr-2 h-5 w-5"
-											aria-hidden="true"
-										/>
-										<span>New Job</span>
-									</button>
-								</div>
-							</div>
-						</div>
-					</div>
+  return (
+    <Disclosure as="nav">
+      {({ open }) => (
+        <>
+          <div
+            className="pt-2"
+            style={{
+              background: 'transparent',
+            }}
+          >
+            <div className="flex justify-between h-16 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+              <div className="flex">
+                <div className="-ml-2 mr-2 flex items-center md:hidden">
+                  {/* Mobile menu button */}
+                  <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                    <span className="sr-only">Open main menu</span>
+                    {open ? (
+                      <XIcon className="block h-6 w-6" aria-hidden="true" />
+                    ) : (
+                      <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                    )}
+                  </Disclosure.Button>
+                </div>
+                <div className="flex-shrink-0 flex items-center">
+                  <img
+                    className="block lg:hidden h-8 w-auto"
+                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                    alt="Workflow"
+                  />
+                  <img
+                    className="hidden lg:block h-8 w-auto"
+                    src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
+                    alt="Workflow"
+                  />
+                </div>
+                <div className="hidden md:ml-6 md:flex md:items-center md:space-x-4">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className={classNames(
+                        'text-gray-300 hover:bg-gray-700 hover:text-white',
+                        'px-3 py-2 rounded-md text-base font-medium'
+                      )}
+                      aria-current={item.current ? 'page' : undefined}
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+              </div>
+              <div className="flex items-center">
+                <div className="flex-shrink-0">
+                  <button
+                    type="button"
+                    className=" w-[200px] inline-flex items-center justify-center py-3 mt-[5%] border-2 text-base font-semibold rounded-full shadow-sm text-white bg-transparent hover:bg-teal-600 hover:border-transparent"
+                  >
+                    Connect Wallet
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
 
-					<Disclosure.Panel className="md:hidden">
-						<div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-							{navigation.map((item) => (
-								<Disclosure.Button
-									key={item.name}
-									as="a"
-									href={item.href}
-									className={classNames(
-										"text-gray-300 hover:bg-gray-700 hover:text-white",
-										"block px-3 py-2 rounded-md text-base font-medium"
-									)}
-									aria-current={
-										item.current ? "page" : undefined
-									}>
-									{item.name}
-								</Disclosure.Button>
-							))}
-						</div>
-					</Disclosure.Panel>
-				</>
-			)}
-		</Disclosure>
-	);
+          <Disclosure.Panel className="md:hidden">
+            <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block px-3 py-2 rounded-md text-base font-medium'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  );
 }
