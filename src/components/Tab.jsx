0204a04078837c269f82e15e5efdb9f159c5c969
kg-kartik <goel.kartik39@gmail.com>
2022-07-12 19:54:32 +0530

    teams section done

---
+++ src/components/Tab.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+
+const tabs = [
+  { name: 'Management' },
+  { name: 'Directors' },
+  { name: 'Developers' },
+  { name: 'Marketing' },
+  { name: 'Artists' },
+  { name: 'Writers' },
+  { name: 'Advisors' },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ');
+}
+
+export default function Tabs({ setVal }) {
+  const [current, setCurrent] = useState('Management');
+
+  return (
+    <div>
+      <div className="sm:hidden flex justify-center">
+        <select
+          id="tabs"
+          name="tabs"
+          onChange={(e) => {
+            setVal(e.target.value);
+            setCurrent(e.target.value);
+          }}
+          className="block w-[150px] h-[50px] rounded-md text-white bg-teal-500 text-gray-200 px-4"
+          defaultValue={current}
+        >
+          {tabs.map((tab) => (
+            <option key={tab.name}>{tab.name}</option>
+          ))}
+        </select>
+      </div>
+      <div className="hidden sm:block">
+        <nav className="flex space-x-4 justify-center" aria-label="Tabs">
+          {tabs.map((tab) => (
+            <a
+              key={tab.name}
+              href={tab.href}
+              onClick={() => {
+                setVal(tab.name);
+                setCurrent(tab.name);
+              }}
+              className={classNames(
+                current === tab.name
+                  ? 'bg-teal-500 text-gray-200'
+                  : 'text-white hover:text-gray-400 hover:cursor-pointer',
+                'px-3 py-2 font-normal text-base rounded-md'
+              )}
+              aria-current={tab.current ? 'page' : undefined}
+            >
+              {tab.name}
+            </a>
+          ))}
+        </nav>
+      </div>
+    </div>
+  );
+}
--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,87 +1,72 @@
-import Tabs from "../Tab";
-
-/*
-  This example requires Tailwind CSS v2.0+ 
-  
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/aspect-ratio'),
-    ],
-  }
-  ```
-*/
-const people = [
-	{
-		name: "Lindsay Walton",
-		role: "Front-end Developer",
-		imageUrl:
-			"https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
-	},
-	{
-		name: "Lindsay Walton",
-		role: "Front-end Developer",
-		imageUrl:
-			"https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
-	},
-	{
-		name: "Lindsay Walton",
-		role: "Front-end Developer",
-		imageUrl:
-			"https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
-	},
-	// More people...
-];
+import { useState } from 'react';
+import Tabs from './Tab';
+import { team } from '../utils/team';
+import { motion } from 'framer-motion';
+import { fadeIn, staggerContainer } from './motionVariants';
 
 export default function Team() {
-	return (
-		<div className="text-white">
-			<div className="mx-auto py-16 max-w-7xl px-12 lg:px-8">
-				<div className="space-y-12">
-					<div className="space-y-5 text-center sm:space-y-4">
-						<h2 className="sm:text-[34px] text-2xl uppercase title-font font-extrabold ">
-							Meet Our Team
-						</h2>
-						<p className="text-[#595959] leading-relaxed">
-							Created by many. Meet them all.
-						</p>
-					</div>
-					<Tabs />
-					<ul
-						role="list"
-						className="space-y-12 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:grid-cols-3 lg:gap-x-8">
-						{people.map((person) => (
-							<li key={person.name}>
-								<div className="space-y-4">
-									{/* <div className="aspect-w-3 aspect-h-2"> */}
-									<img
-										className="object-cover shadow-lg rounded-lg"
-										src={person.imageUrl}
-										alt=""
-									/>
-									{/* </div> */}
+  const [role, setRole] = useState('Management');
 
-									<div className="space-y-2 text-center">
-										<div className="text-lg leading-6 space-y-1">
-											<h3 className="font-medium title-font">
-												{person.name}
-											</h3>
-											<p className="text-[#595959] body-font">
-												{person.role}
-											</p>
-										</div>
-									</div>
-								</div>
-							</li>
-						))}
-					</ul>
-				</div>
-			</div>
-		</div>
-	);
+  const setRoleFunc = (val) => {
+    setRole(val);
+  };
+
+  return (
+    <div className="text-white">
+      <div className="mx-auto py-16 max-w-7xl px-12 lg:px-8">
+        <div className="space-y-12">
+          <div className="space-y-5 text-center sm:space-y-4">
+            <h2 className="sm:text-[34px] text-2xl uppercase title-font font-extrabold ">
+              Meet Our Team
+            </h2>
+            <p className="text-[#595959] leading-relaxed">
+              Created by many. Meet them all.
+            </p>
+          </div>
+          <Tabs setVal={setRoleFunc} />
+          <ul
+            role="list"
+            className="space-y-12 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:grid-cols-3 lg:gap-x-8"
+          >
+            {team
+              .filter((teamP) => teamP.team === role)
+              .map((person) => (
+                <li key={person.name}>
+                  <motion.div
+                    variants={staggerContainer}
+                    animate="animate"
+                    initial="initial"
+                    className="space-y-4"
+                  >
+                    {/* <div className="aspect-w-3 aspect-h-2"> */}
+                    <motion.span variants={fadeIn()}>
+                      <img
+                        className="object-cover shadow-lg rounded-lg"
+                        src={person.imageUrl}
+                        alt={person.imageUrl}
+                      />
+                    </motion.span>
+                    {/* </div> */}
+                    <div className="space-y-2 text-center">
+                      <div className="text-lg leading-6 space-y-1">
+                        <motion.span variants={fadeIn()}>
+                          <h3 className="font-medium title-font">
+                            {person.name}
+                          </h3>
+                        </motion.span>
+                        <motion.span variants={fadeIn()}>
+                          <p className="text-[#595959] body-font">
+                            {person.role}
+                          </p>
+                        </motion.span>
+                      </div>
+                    </div>
+                  </motion.div>
+                </li>
+              ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
