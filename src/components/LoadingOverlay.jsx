039815f90fc967d65c31f148ccce3ec27f30f4b2
neerajp99 <neerajp1999@gmail.com>
2022-07-19 02:11:31 +0530

    Add kernel

--- src/components/LoadingOverlay.jsx
+++ src/components/LoadingOverlay.jsx
@@ -6,10 +6,10 @@ const LoadingOverlay = ({ logo }) => {
     return (
         <>
             <div className="loading-overlay cursor-none overflow-hidden">
-                <div className="loading-top h-screen w-screen bg-red-900 absolute z-1000">
-                    <div className="overlay-top h-screen w-screen absolute bg-black flex justify-center items-center flex-col">
+                <div className="loading-top h-screen w-screen bg-red-900 absolute z-150 overflow-hidden">
+                    <div className="overlay-top h-screen w-screen absolute bg-black flex justify-center items-center flex-col overflow-hidden">
                         <div
-                            className="logoDiv h-40 w-40 xs:-mt-16 sm:-mt-16 md:mt-0 lg:mt-0 xl:mt-0 -mt-16"
+                            className="logoDiv h-40 w-40 xs:-mt-16 sm:-mt-16 md:mt-0 lg:mt-0 xl:mt-0 -mt-16 overflow-hidden"
                             ref={logo}
                         >
                             <img
