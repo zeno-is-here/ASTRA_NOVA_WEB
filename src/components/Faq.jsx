fcd0b84191ee7a264dfbfd639484bfe38fc80cc6
neerajp99 <neerajp1999@gmail.com>
2022-07-20 17:02:14 +0530

    Update

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -70,14 +70,14 @@ export default function Faq() {
                     ))}
                 </dl>
             </div>
-            <div
+            {/* <div
                 // style={{"opacity: 1;transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg);transform-style: preserve-3d;"}}
                 className="background-pattern7"
             ></div>
             <div
                 // style={{"opacity: 1;transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg);transform-style: preserve-3d;"}}
                 className="background-pattern8"
-            ></div>
+            ></div> */}
         </div>
     );
 }
